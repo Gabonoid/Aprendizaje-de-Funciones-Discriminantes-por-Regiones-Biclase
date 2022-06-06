a3bca72fdbb62393ac67bacdb062fe5491826f51
Gabriel Serrano <gabo09link@gmail.com>
2022-06-06 04:26:56 -0500

    obteniendo funcion discriminante Lineal

--- biclase.py
+++ biclase.py
@@ -4,8 +4,5 @@ import numpy as np
 if __name__ == "__main__":
     vectores = fd.generarCMA()
     print(vectores)
-    print(f"Soluciòn final: {fd.calcularFD(vectores)}")
-    
-    #vectorPrueba = [0,0,0]
-    #dotprueba = np.dot(vectores[0][0], vectorPrueba)
-    #print(dotprueba)
+    funDiscLineal = fd.calcularFD(vectores)
+    print(f"Soluciòn final: {funDiscLineal}")
